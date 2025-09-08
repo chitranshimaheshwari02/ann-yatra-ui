f69631cb8e4ad34f0a9a2685b3c0957811c1f497
Chitranshi Maheshwari <chitranshimaheshwari@Chitranshis-MacBook-Air.local>
2025-09-08 22:53:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,3 @@
-// import React from "react";
-// import Navbar from "./components/Navbar";
-// import HeroSection from "./components/HeroSection";
-// import FeaturesSection from "./components/FeaturesSection";
-// import HowItWorks from "./components/HowItWorks";
-// import Stakeholders from "./components/Stakeholders";
-// import DashboardSection from "./components/DashboardSection";
-
-// import "./styles/HeroSection.scss";
-// import "./styles/Navbar.scss";
-// import "./styles/FeaturesSection.scss";
-// import "./styles/HowItWorks.scss";
-// import "./styles/Stakeholders.scss";
-// import "./styles/DashboardSection.scss";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <HeroSection />
-//       <FeaturesSection />
-//       <HowItWorks />
-//       <Stakeholders />
-//       <DashboardSection />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
